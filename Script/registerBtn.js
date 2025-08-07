c0c6788828ef08709f52038401ec76bee54aadc8
Wiratata <wiranata.glitch@gmail.com>
2025-08-07 12:24:10 -0600

    update the link of the page

--- Script/registerBtn.js
+++ Script/registerBtn.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#registerBtn').click(function() {
-        window.location.href = './RegisterPage.html';
+        window.location.href = './RegistrationPage.html';
     });
 });
